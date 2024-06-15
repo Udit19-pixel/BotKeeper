85abc4c274c4f288b17a3518d806f11ecc64c210
Udit Gaur <83877590+Udit19-pixel@users.noreply.github.com>
2024-06-15 11:59:25 +0530

    Update System_tray.py

--- BotKeeper/System_tray.py
+++ BotKeeper/System_tray.py
@@ -2,7 +2,6 @@ from pystray import Icon, MenuItem
 from PIL import Image
 import subprocess
 
-# Path to your Docker Compose file
 DOCKER_COMPOSE_PATH = "D:/Python/BotKeeper/docker-compose.yml"
 
 def start_container(icon, item):
@@ -14,22 +13,17 @@ def stop_container(icon, item):
     icon.visible = False
 
 def main():
-    # Load icon image
     image = Image.open("Process.png")
 
-    # Create icon
     icon = Icon("Chatbot", image)
 
-    # Create menu items
     menu_items = [
         MenuItem('Start', lambda icon, item: start_container(icon, item)),
         MenuItem('Stop', lambda icon, item: stop_container(icon, item))
     ]
 
-    # Set menu for icon
     icon.menu = menu_items
-
-    # Run icon in system tray
+    
     icon.run()
 
 if __name__ == "__main__":
